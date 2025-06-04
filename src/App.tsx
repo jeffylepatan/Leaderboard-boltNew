2d241069fe84aa127c9326b8114106095a964e30
jeffylepatan <47520845+jeffylepatan@users.noreply.github.com>
2025-06-04 13:14:24 +0800

    Integrate Google Authentication

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,26 @@
 import React from 'react';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 import Leaderboard from './components/Leaderboard';
 import Fireflies from './components/Fireflies';
 import MagicCircles from './components/MagicCircles';
+import Login from './components/Login';
 
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = React.useState(false);
+
   return (
-    <div className="min-h-screen bg-black from-indigo-100 to-purple-100 flex flex-col items-center justify-start py-8 relative">
-      <MagicCircles />
-      <Fireflies />
-      <div className="relative z-10 w-full">
-        <Leaderboard />
+    <GoogleOAuthProvider clientId="YOUR_GOOGLE_CLIENT_ID">
+      <div className="min-h-screen bg-black from-indigo-100 to-purple-100 flex flex-col items-center justify-start py-8 relative">
+        <MagicCircles />
+        <Fireflies />
+        <div className="relative z-10 w-full">
+          {isAuthenticated ? (
+            <Leaderboard />
+          ) : (
+            <Login onLogin={() => setIsAuthenticated(true)} />
+          )}
+        </div>
       </div>
-    </div>
+    </GoogleOAuthProvider>
   );
 }
-
-export default App;
